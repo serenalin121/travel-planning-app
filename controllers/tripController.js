b3d8dd2cf648e2a137c822b618df2869ac53f67f
serenalin121 <serenalin121@gmail.com>
2021-09-20 13:49:07 -0700

    Index Working

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Trip = require("../models/trip");
 
 router.get("/", (req, res) => {
-  res.send("Yeah!!! you hit the trip homepage");
+  res.render("trips/index.ejs");
 });
 
 module.exports = router;
